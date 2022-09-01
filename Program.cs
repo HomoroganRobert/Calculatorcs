8b1ce9f7876fa5f365036b150dbab36e24a3b655
Robert Homorogn <rohomorogan@gmail.com>
2022-09-01 14:50:31 +0300

    Calculator Finish
    Calculator Finish

--- Program.cs
+++ Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Char op;
+            Double num1, num2;
+            
+            Console.WriteLine("Enter the operator (+, -, *, /)");
+            op = Console.ReadLine()[0];
+
+            Console.WriteLine("Enter 2 numbers one by one");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            switch( op)
+            {
+                case'+':
+                    Console.WriteLine("{0} + {1} = {2}", num1,num2,(num1+num2));
+                    break;
+
+                case'-':
+                    Console.WriteLine("{0} - {1} = {2}", num1,num2,(num1-num2));
+                    break;
+
+                case'*':
+                    Console.WriteLine("{0} * {1} = {2}", num1,num2,(num1*num2));
+                    break;
+
+                case'/':
+                    if (num2 == 0.0)
+                        Console.WriteLine("Diveded by 0 situation");
+                    else
+                        Console.WriteLine("{0} / {1} = {2}", num1,num2,(num1/num2));
+                    break;
+
+                default:
+                    Console.WriteLine("{0} is an invalid operator",op);
+                    break;
+
+            }
+
+            Console.WriteLine("Press any key to exit de program");
+            Console.ReadKey();
+        }
+    }
+}
